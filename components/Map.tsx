4df713282c17fbacd77b56fb6ecd4bfdf57af0b1
Jasmine Dillard <jdilldev@gmail.com>
2023-03-20 15:23:37 -0400

    trying map in memo

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Map as Mapbox, MapRef, MapLayerMouseEvent } from "react-map-gl";
-import { useCallback, useContext, useRef, useState } from "react";
+import { memo, useCallback, useContext, useRef, useState } from "react";
 import { useWindowSize } from "../app/hooks/hooks";
 import { useRouter } from "next/navigation";
 import { replaceSpacesWithUnderscore } from "../utils";
@@ -9,7 +9,7 @@ import { usePathname } from "next/navigation";
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 
-const Map = () => {
+const Map = memo(() => {
     const router = useRouter();
     const pathname = usePathname()
     const [zoom, setZoom] = useState(0)
@@ -20,9 +20,38 @@ const Map = () => {
 
     const [_, theme, __, metric] = pathname!.split('/')
 
-    return <div className={'h-full w-full relative md:ml-20 md:h-[90%]'}>
-        <p>{theme}</p>
-    </div>
-}
+    return <Mapbox
+        trackResize
+        minZoom={1}
+        maxZoom={2.5}
+        // zoom={zoom}
+        mapboxAccessToken='pk.eyJ1IjoiamRpbGxkZXYiLCJhIjoiY2xjbHR0MXNtOXE3ZTN2cGx1YWwxYmE4cyJ9.UKQMbbf2Q4revc3Nz9ws3g'
+        initialViewState={{
+            longitude,
+            latitude,
+            zoom: 1,
+        }}
+        interactiveLayerIds={['subregional-data']}
+        projection={projection}
+        attributionControl={false}
+        style={{ position: 'absolute', width: '100%', height: '100%', }}
+        mapStyle="mapbox://styles/jdilldev/clemtp805000901s45xextcln"
+        onClick={(e: MapLayerMouseEvent) => {
+            if (theme === '' || !e.features || !e.features[0]) return
+            const { properties } = e.features[0];
+            const subregion = properties!.subregionName
+
+            let route = `${theme}/singleRegion/${metric}?region=${subregion}`
+            route = replaceSpacesWithUnderscore(route)
+            router.push(route, {})
+            e.target.flyTo({ center: [e.lngLat.lng, e.lngLat.lat], duration: 1000 });
+
+        }}
+    //onZoom={(e) => { console.log(e) }}
+    /*            onDragEnd={(e) => {
+                   setRegion('')
+               }} */
+    />
+}, true)
 
 export default Map
