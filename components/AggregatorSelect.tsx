4452b0b36c42cfe0d6bfee6b9822c94475d56bf5
Jasmine Dillard <jdilldev@gmail.com>
2023-03-21 20:39:32 -0400

    move select theme on mobile back to top right

--- components/AggregatorSelect.tsx
+++ components/AggregatorSelect.tsx
@@ -13,7 +13,7 @@ const AggregatorSelect = () => {
 
     const [_, theme, grouping, metric] = pathname!.split('/')
 
-    return !theme ? <p className="md:hidden font-equinox w-full text-center lowercase z-10 fixed right-2 top-14 text-red-500">{DEFAULT_THEME_PROMPT}</p>
+    return !theme ? <p className="md:hidden font-equinox text-center lowercase z-10 fixed right-2 top-14 text-red-500">{DEFAULT_THEME_PROMPT}</p>
         : <select
             onChange={(e) => router.push(`/${theme}/${e.target.value}/${metric}${region ? '?region=' + region : ''}`)}
             className="z-10 fixed right-2 top-12 md:static md:text-sm text-teal-500 md:-ml-1 md:text-pink-500 bg-transparent w-fit  whitespace-pre-wrap"
