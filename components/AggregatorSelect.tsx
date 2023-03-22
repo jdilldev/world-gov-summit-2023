906d447bc926b7b147837b4b71c2ea795f725efe
Jasmine Dillard <jdilldev@gmail.com>
2023-03-21 20:28:27 -0400

    revert width change to aggregator select

--- components/AggregatorSelect.tsx
+++ components/AggregatorSelect.tsx
@@ -16,12 +16,12 @@ const AggregatorSelect = () => {
     return !theme ? <p className="md:hidden font-equinox w-full text-center lowercase z-10 fixed right-2 top-14 text-red-500">{DEFAULT_THEME_PROMPT}</p>
         : <select
             onChange={(e) => router.push(`/${theme}/${e.target.value}/${metric}${region ? '?region=' + region : ''}`)}
-            className="z-10 w-full fixed right-2 top-12 md:static md:text-sm text-teal-500 md:-ml-1 md:text-pink-500 bg-transparent whitespace-pre-wrap"
+            className="z-10 fixed right-2 top-12 md:static md:text-sm text-teal-500 md:-ml-1 md:text-pink-500 bg-transparent w-fit  whitespace-pre-wrap"
             placeholder="Select Grouping"
             value={grouping}>
             <option value='world'>Worldwide</option>
             <option value='allRegions'>By Region</option>
-            {region && grouping === 'singleRegion' && <option value='singleRegion'>{replaceUnderscoreWithSpace(region)}</option>}
+            {grouping === 'singleRegion' && region && <option value='singleRegion'>{replaceUnderscoreWithSpace(region)}</option>}
         </select>
 }
 
